cf2bfe8b71ae1722c79eeeee80d173edc3908fc3
asd147asd147 <55697983+asd147asd147@users.noreply.github.com>
2020-11-01 19:14:42 +0900

    delete

--- CCMBack/README.md
+++
@@ -1,2 +0,0 @@
-# PNU CCM No.1 ccmWelcome
-## Choi Won June, Ahan Hyun Ju, Kim Tae Ho
--- CCMBack/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const user = require('./api/user');
-
-router.use('/user', user);
-
-
-module.exports = router;
