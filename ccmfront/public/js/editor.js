e6e788f76c70da379b684e753a81073f87c6e5f5
asd147asd147 <55697983+asd147asd147@users.noreply.github.com>
2020-11-01 18:25:10 +0900

    Make Source Code Editor
    Make Source Code Editor

---
+++ ccmfront/public/js/editor.js
@@ -0,0 +1,40 @@
+const executeCodeBtn = document.querySelector('.editor__run');
+const resetCodeBtn = document.querySelector('.editor__reset');
+
+let codeEditor = ace.edit("editorCode");
+let defaultCode = 'console.log("Hello World!")';
+let consoleMessages = [];
+
+let  editorLib = {
+    init(){
+        codeEditor.setTheme("ace/theme/dracula");
+
+        codeEditor.session.setMode("ace/mode/python");
+
+        codeEditor.setOptions({
+            fontFamily: 'Inconsolata',
+            fontSize: '15pt',
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+        });
+
+        codeEditor.setValue(defaultCode);
+    }
+}
+
+executeCodeBtn.addEventListener('click', () => {
+
+    const userCode = codeEditor.getValue();
+
+    try {
+        new Function(userCode)();
+    } catch (err){
+        console.log(err);
+    }
+});
+
+resetCodeBtn.addEventListener('click', ()=>{
+    codeEditor.setValue(defaultCode);
+})
+
+editorLib.init();
