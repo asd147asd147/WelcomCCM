0abdda3d0156c709f9f7ef289d8f5cd054a02652
Train0303 <39511892+Train0303@users.noreply.github.com>
2020-11-02 20:55:32 +0900

    11/01

--- ccmback/compiler/compile_py.js
+++ ccmback/compiler/compile_py.js
@@ -1,14 +1,20 @@
 var process = require('child_process');
 var fs = require('fs');
-
-process.exec('python ./data/user.py',function (err,stdout,stderr) {
+var memberData = new Object();
+memberData.name="user"
+memberData.InputFile=memberData.name+".py"
+memberData.OutputFile=memberData.name+".txt"
+memberData.result = ""
+process.exec('python ./data/'+memberData.InputFile,function (err,stdout,stderr) {
     if (err) {
-        fs.writeFile('./result/user.txt',stderr,'utf-8',function(e){
+        fs.writeFile('./result/'+ memberData.OutputFile,stderr,'utf-8',function(e){
             if(e){
                 console.log(e);
             }
             else{
                 console.log('02 WRITE DONE!');
+                memberData.result = stderr;
+                console.log(JSON.stringify(memberData));
             }
         });
     } else {
@@ -18,6 +24,8 @@ process.exec('python ./data/user.py',function (err,stdout,stderr) {
             }
             else{
                 console.log('02 WRITE DONE!');
+                memberData.result = stdout;
+                console.log(JSON.stringify(memberData));
             }
         });
     }
