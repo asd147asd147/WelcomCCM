186fedd532bac0551989ad3f2a6a5cc5ee0dacac
asd147asd147 <55697983+asd147asd147@users.noreply.github.com>
2020-11-03 23:59:10 +0900

    front compiler REST 통신
    front compiler REST 통신

--- ccmfront/public/js/editor.js
+++ ccmfront/public/js/editor.js
@@ -1,4 +1,4 @@
-
+const consoleLogList = document.querySelector('.editor__console-logs');
 const executeCodeBtn = document.querySelector('.editor__run');
 const resetCodeBtn = document.querySelector('.editor__reset');
 
@@ -6,10 +6,31 @@ let codeEditor = ace.edit("editorCode",{
     mode : "ace/mode/python",
     selectionStyle: "text",
 });
+
 let defaultCode = 'print("Hello World!")';
 let consoleMessages = [];
 
 let  editorLib = {
+    clearConsoleScreen(){
+        consoleMessages.length = 0;
+
+        while(consoleLogList.firstChild){
+            consoleLogList.removeChild(consoleLogList.firstChild);
+        }
+    },
+    printToConsole(){
+        consoleMessages.forEach(log => {
+            const newLogItem = document.createElement('li');
+            const newLogText = document.createElement('pre');
+
+            newLogText.className = log.class;
+            newLogText.textContent = `> ${log.message}`;
+
+            newLogItem.appendChild(newLogText);
+
+            consoleLogList.appendChild(newLogItem);
+        })
+    },
     init(){
         codeEditor.setTheme("ace/theme/dracula");
 
@@ -29,18 +50,22 @@ let  editorLib = {
 }
 
 executeCodeBtn.addEventListener('click', () => {
-
+    editorLib.clearConsoleScreen();
     const userCode = codeEditor.getValue();
     var jsonfile = JSON.stringify(userCode);
     try {
-        console.log(jsonfile);
+
+        // console.log(jsonfile);
+        // new Function(userCode)();
     } catch (err){
         console.log(err);
     }
+    editorLib.printToConsole();
 });
 
 resetCodeBtn.addEventListener('click', ()=>{
     codeEditor.setValue(defaultCode);
+    editorLib.clearConsoleScreen();
 })
 
 editorLib.init();
--- ccmfront/src/App.js
+++ ccmfront/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Posts from './components/Posts';
 
 class App extends Component{
   render(){
       return (
-        <div className="App"></div>
+        <div className="App">
+          <Posts />
+        </div>
       );
     }
 }
