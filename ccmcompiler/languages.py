c29a70c67db90cf7f603f3f84c67e801b26660cc
Train0303 <39511892+Train0303@users.noreply.github.com>
2020-11-04 23:32:39 +0900

    make All_compile maybe? (#10)
    * 10/30
    
    * 11/01
    
    * Update user.py
    
    * 11/02
    
    * 11/03
    
    * 11/03
    
    * 11/04
    
    * 11/04 02:11
    
    * 11/04 02:24
    
    * make All_compile
    
    * make All_compile

--- ccmcompiler/languages.py
+++ ccmcompiler/languages.py
@@ -1,39 +1,53 @@
 import os
-c_language ={"compile":
-                {   "src_path" : os.path.abspath('./data/main.c'),
-                    "exe_path" : os.path.abspath('./data')+'/main',
-                    "max_real_time" : 10000,
-                    "max_memory" : 256 * 1024 * 1024,
-                    "compile_cmd" : "gcc -o {exe_path} {src_path}"
-                },
-             "run":
-                {"command" : "{exe_path}" 
-                }
-            }
-
-cpp_language ={"compile":
-                {   "src_path" : os.path.abspath('./data/main.cpp'),
-                    "exe_path" : os.path.abspath('./data')+'/main',
-                    "max_real_time" : 10000,
-                    "max_memory" : 256 * 1024 * 1024,
-                    "compile_cmd" : "g++ -o {exe_path} {src_path}"
-                },
-             "run":
-                {"command" : "{exe_path}" 
-                }
-            }
-
-py3_language = {"compile" :
-                    {
-                        "src_path" : os.path.abspath('./data/user.py'),
-                        "exe_path" : os.path.abspath('./data')+'/user.pyc',
-                        "max_real_time": 10000,
-                        "max_memory": 128 * 1024 * 1024,
-                        "compile_cmd" : "python3 -m py_compile {src_path}"
-                    }
-                ,
+class language:
+    compile_language = dict()
+    c_language ={"compile":
+                    {   "src_path" : os.path.abspath('./data/main.c'),
+                        "exe_path" : os.path.abspath('./data')+'/main',
+                        "max_real_time" : 10000,
+                        "max_memory" : 256 * 1024 * 1024,
+                        "compile_cmd" : "gcc",
+                        "arg1" : "-o {exe_path} {src_path}"
+                    },
                 "run":
-                    {
-                        "command" : "python3 {exe_path}"
+                    {"command" : "{exe_path}" 
                     }
                 }
+
+    cpp_language ={"compile":
+                    {   "src_path" : os.path.abspath('./data/main.cpp'),
+                        "exe_path" : os.path.abspath('./data')+'/main',
+                        "max_real_time" : 10000,
+                        "max_memory" : 256 * 1024 * 1024,
+                        "compile_cmd" : "g++",
+                        "arg1" : "-o {exe_path} {src_path}"
+                    },
+                "run":
+                    {"command" : "{exe_path}" 
+                    }
+                }
+
+    py3_language = {"compile" :
+                        {
+                            "src_path" : os.path.abspath('./data/user.py'),
+                            "exe_path" : os.path.abspath('./data')+'/user.pyc',
+                            "max_real_time": 10000,
+                            "max_memory": 128 * 1024 * 1024,
+                            "compile_cmd" : "python",
+                            "arg1" : "-m {src_path}"
+                        }
+                    ,
+                    "run":
+                        {
+                            "command" : "python",
+                            "arg1" : "{exe_path}"
+                        }
+                    }
+    def __init__(self, select):
+        if(select == "python"):
+            self.compile_language = self.py3_language
+        elif(select == "c"):
+            self.compile_language = self.c_language
+        elif(select == "cpp"):
+            self.compile_language = self.cpp_language
+
