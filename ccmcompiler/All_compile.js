81e5f6202e700ae3d20b2977601256fb1c80d8a5
Train0303 <39511892+Train0303@users.noreply.github.com>
2020-11-05 22:21:15 +0900

    11/05 complete timechecker, errorcode (#11)
    * 11/05 middle check

--- ccmcompiler/All_compile.js
+++ ccmcompiler/All_compile.js
@@ -1,26 +1,53 @@
 var fs = require('fs');
 var process = require('child_process');
 
-var select = "python"
+var select = "c"
 // select c++, c ~~
-process.execFile('./dist/compile',[select],function (err,stdout,stderr) {
-    if(err) {
-        fs.writeFile('./result/user.txt',stderr,'utf-8',function(e){
-            if(e){
-                console.log(e);
-            }
-            else{
-                console.log('02 WRITE DONE!');
-            }
-        });
-    } else {
-        fs.writeFile('./result/user.txt',stdout,'utf-8',function(e){
-            if(e){
-                console.log(e);
-            }
-            else{
-                console.log('02 WRITE DONE!');
-            }
-        });
-    }
-});
+var time_out = 1
+
+if(select == "python"){
+    process.execFile('./dist/compile_py',[select,time_out],function (err,stdout,stderr) {
+        if(err) {
+            fs.writeFile('./result/result.txt',stderr,'utf-8',function(e){
+                if(e){
+                    console.log(e);
+                }
+                else{
+                    console.log('02 WRITE DONE!');
+                }
+            });
+        } else {
+            fs.writeFile('./result/result.txt',stdout,'utf-8',function(e){
+                if(e){
+                    console.log(e);
+                }
+                else{
+                    console.log('02 WRITE DONE!');
+                }
+            });
+        }
+    });
+}
+else{
+    process.execFile('./dist/compile_c_cpp',[select,time_out],function (err,stdout,stderr) {
+        if(err) {
+            fs.writeFile('./result/result.txt',stderr,'utf-8',function(e){
+                if(e){
+                    console.log(e);
+                }
+                else{
+                    console.log('02 WRITE DONE!');
+                }
+            });
+        } else {
+            fs.writeFile('./result/result.txt',stdout,'utf-8',function(e){
+                if(e){
+                    console.log(e);
+                }
+                else{
+                    console.log('02 WRITE DONE!');
+                }
+            });
+        }
+    });
+}
--- ccmcompiler/languages.py
+++ ccmcompiler/languages.py
@@ -6,25 +6,27 @@ class language:
                         "exe_path" : os.path.abspath('./data')+'/main',
                         "max_real_time" : 10000,
                         "max_memory" : 256 * 1024 * 1024,
-                        "compile_cmd" : "gcc",
-                        "arg1" : "-o {exe_path} {src_path}"
+
+                        "compile_cmd" : "gcc -o "+os.path.abspath('./data')+'/main'+" "+ os.path.abspath('./data/main.c'),
+                        
                     },
                 "run":
-                    {"command" : "{exe_path}" 
-                    }
+                    {"command" : os.path.abspath('./data')+'/main'
+                    },
+                "lang" : "c"
                 }
 
     cpp_language ={"compile":
-                    {   "src_path" : os.path.abspath('./data/main.cpp'),
-                        "exe_path" : os.path.abspath('./data')+'/main',
+                    {   "src_path" : os.path.abspath('./data/main_cpp.cpp'),
+                        "exe_path" : os.path.abspath('./data')+'/main_cpp',
                         "max_real_time" : 10000,
                         "max_memory" : 256 * 1024 * 1024,
-                        "compile_cmd" : "g++",
-                        "arg1" : "-o {exe_path} {src_path}"
+                        "compile_cmd" : "g++ -o "+os.path.abspath('./data')+'/main_cpp'+" "+ os.path.abspath('./data/main_cpp.cpp'),
                     },
                 "run":
-                    {"command" : "{exe_path}" 
-                    }
+                    {"command" : os.path.abspath('./data')+'/main_cpp' 
+                    },
+                "lang" : "cpp"
                 }
 
     py3_language = {"compile" :
@@ -33,16 +35,16 @@ class language:
                             "exe_path" : os.path.abspath('./data')+'/user.pyc',
                             "max_real_time": 10000,
                             "max_memory": 128 * 1024 * 1024,
-                            "compile_cmd" : "python",
-                            "arg1" : "-m {src_path}"
+                            "compile_cmd" : "python "+ os.path.abspath('./data/user.py')
                         }
                     ,
                     "run":
                         {
-                            "command" : "python",
-                            "arg1" : "{exe_path}"
-                        }
+                            "command" : "python "+ os.path.abspath('./__pycache__/')+" "+ '/user.cpython-37.pyc',
+                        },
+                    "lang" : "python"    
                     }
+
     def __init__(self, select):
         if(select == "python"):
             self.compile_language = self.py3_language
